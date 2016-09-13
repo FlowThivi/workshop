a5222fe30dd85adead0197146999b2c2686c4af1
Pierre Cabrière <pierre.cabriere@gmail.com>
2016-09-13 17:16:00 +0200

    add profil image component

---
+++ src/app/settings/settings-main/settings-main-image/settings-main-image.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { SettingsMainImageComponent } from './settings-main-image.component';
+
+describe('Component: SettingsMainImage', () => {
+  it('should create an instance', () => {
+    let component = new SettingsMainImageComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/settings/settings-main/settings-main-image/settings-main-image.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-settings-main-image',
+  templateUrl: './settings-main-image.component.html',
+  styleUrls: ['./settings-main-image.component.scss']
+})
+export class SettingsMainImageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
